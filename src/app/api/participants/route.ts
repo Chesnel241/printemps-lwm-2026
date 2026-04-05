0b7de9d965c64f7c53bf9ecd422692d075d15831
EKOGHA Chesnel <122566856+Chesnel241@users.noreply.github.com>
2026-04-05 07:55:12 +0200

    Delete src directory

--- src/app/api/participants/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { AuthService } from '@/backend/services/auth.service'
-
-export const dynamic = 'force-dynamic'
-
-export async function GET() {
-  const parts = await AuthService.getParticipants()
-  return NextResponse.json(parts)
-}
